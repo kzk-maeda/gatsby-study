3593427495371e8120b78378ab2db7808430d02f
kzk-maeda <kzk.maeda0711@gmail.com>
2020-12-31 21:08:46 +0900

    component css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/header'
 
-export default function About() {
+export default function RawAbout() {
     return (
         <div style={{ color: `teal` }}>
             <Header headerText="About Gatsby" />
