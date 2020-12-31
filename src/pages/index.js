386678ee5ef6486f26cc6ae878dc98091ab6569a
kzk-maeda <kzk.maeda0711@gmail.com>
2020-12-31 22:08:14 +0900

    component layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
-import { Link } from 'gatsby'
 import Header from './components/header'
+import Layout from '../components/layout'
 
 export default function Home() {
-  return <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello Gatsby!" />
-    <p>what a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  return (
+    <Layout>
+      <div style={{ color: `purple` }}>
+        <Header headerText="Hello Gatsby!" />
+        <p>what a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      </div>
+    </Layout>
+  )
 }
