f7fca376d9cfa2e8417d9a829a4894f94c55b2c6
kzk-maeda <kzk.maeda0711@gmail.com>
2021-01-01 02:52:10 +0900

    graphql to local file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 module.exports = {
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      }
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
